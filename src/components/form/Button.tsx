574edfa70b28216afafb83088a38b0d34160ea4c
Jhoseph Poma <japoma_200@hotmail.com>
2023-06-07 01:37:21 -0500

    children

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -3,10 +3,6 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button = ({ className, ...props }: ButtonProps) => {
-	return (
-		<button {...props} type='submit' className={`rounded-lg px-3 py-2 ${className || ''}`}>
-			Login
-		</button>
-	);
+	return <button {...props} type='submit' className={`rounded-lg px-3 py-2 ${className || ''}`} />;
 };
 export default Button;
