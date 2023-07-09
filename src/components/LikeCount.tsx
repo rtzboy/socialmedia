e5cf78ba1f643f6fce04e7783dbd915657f68ce2
Jhoseph Poma <japoma_200@hotmail.com>
2023-07-09 14:22:55 -0500

    likeCount re-render avoided

--- src/components/LikeCount.tsx
+++ src/components/LikeCount.tsx
@@ -1,36 +1,27 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
 
 type LikeCountProps = {
 	count: number;
 	likeStatus: boolean;
 };
 
-const LikeCount = ({ count, likeStatus }: LikeCountProps) => {
-	const [likeCountChange, setLikeCountChange] = useState(count);
-
-	useEffect(() => {
-		setLikeCountChange(count);
-	}, [count]);
-
-	return (
-		<AnimatePresence>
-			<motion.span
-				className='absolute left-5'
-				initial={{ y: likeStatus ? 20 : -20, opacity: 0 }}
-				animate={{ y: 0, opacity: 1 }}
-				exit={{ y: likeStatus ? 20 : -20, opacity: 0, transition: { duration: 0.5 } }}
-				key={count}
-				transition={{
-					y: {
-						type: 'just'
-					}
-				}}
-			>
-				{likeCountChange}
-			</motion.span>
-		</AnimatePresence>
-	);
-};
+const LikeCount = ({ count, likeStatus }: LikeCountProps) => (
+	<AnimatePresence>
+		<motion.span
+			className='absolute left-5'
+			initial={{ y: likeStatus ? 20 : -20, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			exit={{ y: likeStatus ? 20 : -20, opacity: 0, transition: { duration: 0.5 } }}
+			key={count}
+			transition={{
+				y: {
+					type: 'just'
+				}
+			}}
+		>
+			{count}
+		</motion.span>
+	</AnimatePresence>
+);
 
 export default LikeCount;
