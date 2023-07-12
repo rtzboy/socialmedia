31b79d99a50fa41a9e82a853ba4c9a924ea3fbe3
Jhoseph Poma <japoma_200@hotmail.com>
2023-07-12 00:00:18 -0500

    create userGlobal info levelup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 import Navbar from './components/Navbar';
 import { PrivateRoutes, PublicRoutes } from './constants/routes';
+import { createUserInfo } from './features/user/userInfo-slice';
 import { AuthGuard } from './guards/auth.guard';
+import { userInformation } from './lib/api/user/user.api';
 import HomeScreen from './pages/Home/HomeScreen';
 import Login from './pages/Login/Login';
 import UserProfile from './pages/Profiles/UserProfile';
 import Signup from './pages/Signup/Signup';
 
 const App = () => {
-	const isAuthenticated = useAppSelector(state => state.user);
+	const { token, id } = useAppSelector(state => state.user);
+	const { userInfo } = useAppSelector(state => state.userGlobalInfo);
+	const dispatchApp = useAppDispatch();
+
+	const settingUserInfoGlobal = async () => {
+		const { result } = await userInformation(token, id);
+		if (result !== null) {
+			dispatchApp(createUserInfo(result));
+		}
+	};
+	useEffect(() => {
+		if (userInfo._id) return;
+		settingUserInfoGlobal();
+	}, []);
 
 	return (
 		<BrowserRouter>
-			{isAuthenticated.username && <Navbar />}
+			{id && <Navbar />}
 			<Routes>
 				<Route path='/' element={<Navigate to={PrivateRoutes.Home} />} />
 				<Route path={`/${PublicRoutes.Login}`} element={<Login />} />
