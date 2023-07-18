68b26f173940c60dfb814daa9613758df4ea9ef4
Jhoseph Poma <japoma_200@hotmail.com>
2023-07-18 17:41:02 -0500

    previewPic updated

--- src/components/userprof/HeaderProfile.tsx
+++ src/components/userprof/HeaderProfile.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FiCamera } from 'react-icons/fi';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { landscapeTest } from '../../assets';
@@ -6,6 +7,9 @@ import { followState } from '../../features/user/userInfo-slice';
 import { userFollow } from '../../lib/api/user/user.api';
 import { UserPrivateInfo } from '../../types/user.model';
 import Button from '../form/Button';
+import StyledIcon from '../icons/StyledIcon';
+import Modal from '../modals/Modal';
+import UpdateProfilePic from './UpdateProfilePic';
 
 type Props = {
 	userHeader: UserPrivateInfo;
@@ -18,6 +22,7 @@ const HeaderProfile = ({ userHeader, postCount, followStatus, privProfile }: Pro
 	const { token } = useAppSelector(state => state.user);
 	const dispatchApp = useAppDispatch();
 	const [disabled, setDisabled] = useState(false);
+	const [contentModal, setContentModal] = useState<JSX.Element | undefined>();
 	const { idUserParam } = useParams();
 
 	const handleFollowUpdate = async () => {
@@ -32,6 +37,7 @@ const HeaderProfile = ({ userHeader, postCount, followStatus, privProfile }: Pro
 
 	return (
 		<div className='relative overflow-hidden rounded-lg bg-slate-100'>
+			<Modal>{contentModal}</Modal>
 			<div
 				style={{
 					backgroundImage: `url("${landscapeTest}")`,
@@ -42,11 +48,25 @@ const HeaderProfile = ({ userHeader, postCount, followStatus, privProfile }: Pro
 			/>
 			<div className='relative mt-8 rounded-lg p-4'>
 				<div className='flex justify-center'>
-					<img
-						src={userHeader.profilePic}
-						alt={userHeader.username}
-						className='h-[150px] w-[150px] rounded-full'
-					/>
+					<div className='relative h-[150px] w-[150px]'>
+						<img
+							src={userHeader.profilePic}
+							alt={userHeader.username}
+							className='absolute left-0 top-0 h-[150px] w-[150px] rounded-full object-cover'
+						/>
+						{privProfile && (
+							<StyledIcon
+								onClick={() =>
+									setContentModal(
+										<UpdateProfilePic closeModal={() => setContentModal(undefined)} />
+									)
+								}
+								icon={FiCamera}
+								className='absolute bottom-1 right-1 cursor-pointer rounded-full bg-slate-200 p-2'
+								stroke='stroke-[1.5]'
+							/>
+						)}
+					</div>
 				</div>
 				<div className='py-2 text-center text-2xl font-semibold'>{userHeader.username}</div>
 				<div className='relative flex w-full justify-between'>
---
+++ src/components/userprof/UpdateProfilePic.tsx
@@ -0,0 +1,124 @@
+import { ChangeEvent, useRef, useState } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { HiOutlineUserCircle, HiXMark } from 'react-icons/hi2';
+import Button from '../form/Button';
+import StyledIcon from '../icons/StyledIcon';
+
+type Props = {
+	closeModal: () => void;
+};
+
+const MAX_SIZE = 1024 * 300;
+const FILE_TYPE = ['image/png', 'image/jpeg'];
+
+type PreviewType = {
+	fileName: string;
+	src: string;
+	error: string;
+};
+
+const UpdateProfilePic = ({ closeModal }: Props) => {
+	const [preview, setPreview] = useState<PreviewType | null>(null);
+	const inputFileRef = useRef<HTMLInputElement>(null);
+
+	const handlePreviewPic = async (evt: ChangeEvent<HTMLInputElement>) => {
+		let file = evt.target.files?.['0'];
+		if (!file) return setPreview(null);
+		if (!FILE_TYPE.includes(file.type))
+			return setPreview({ src: '', fileName: '', error: 'Only PNG/JPG' });
+		if (file.size > MAX_SIZE)
+			return setPreview({ src: '', fileName: '', error: 'IMG to large (max 200Kb)' });
+
+		try {
+			const resultImg = await imgReader(file);
+			setPreview({ src: resultImg, fileName: file.name, error: '' });
+		} catch (error: any) {
+			setPreview({ src: '', fileName: '', error });
+		}
+	};
+
+	let showMessage = msgAndName(preview);
+
+	return (
+		<div className='relative flex flex-col gap-4 rounded-lg bg-slate-100 p-4'>
+			<div className='text-center text-lg font-semibold'>Update profile picture</div>
+			<StyledIcon
+				icon={HiXMark}
+				onClick={closeModal}
+				size='1.5rem'
+				className='absolute right-3 top-3 cursor-pointer rounded-full bg-slate-200 p-1 hover:bg-slate-300'
+			/>
+			<div className='relative mx-auto'>
+				<div className='h-[250px] w-[250px] border'>
+					<div className='absolute left-0 top-0 flex h-[250px] w-[250px] items-center justify-center text-slate-600'>
+						{preview?.src && (
+							<img
+								src={preview?.src}
+								className='absolute z-10 h-[250px] w-[250px] object-cover opacity-60'
+							/>
+						)}
+						{imgPreview(preview)}
+					</div>
+				</div>
+				<input
+					ref={inputFileRef}
+					onChange={handlePreviewPic}
+					type='file'
+					name=''
+					id=''
+					accept={FILE_TYPE.join(', ')}
+					className='invisible absolute appearance-none'
+				/>
+			</div>
+			{showMessage}
+			<div className='flex justify-center gap-4'>
+				<Button
+					onClick={() => inputFileRef.current?.click()}
+					className='flex items-center gap-2 bg-blue-500 text-white hover:opacity-80'
+				>
+					<StyledIcon icon={AiOutlinePlus} />
+					<span>Upload photo</span>
+				</Button>
+				<Button disabled={!preview?.fileName} className='bg-slate-300 disabled:opacity-50'>
+					Save
+				</Button>
+			</div>
+		</div>
+	);
+};
+const imgPreview = (preview: PreviewType | null) => {
+	if (!preview) return <StyledIcon icon={HiOutlineUserCircle} size='14rem' stroke='stroke-[1]' />;
+	if (!preview.src)
+		return <StyledIcon icon={HiOutlineUserCircle} size='14rem' stroke='stroke-[1]' />;
+	return (
+		<img
+			src={preview.src}
+			alt={preview.fileName}
+			className='absolute h-[250px] w-[250px] rounded-full object-cover'
+		/>
+	);
+};
+
+const msgAndName = (preview: PreviewType | null) => {
+	if (!preview) return <div className='text-center font-semibold italic'>JPG/PNG | Max. 200Kb</div>;
+	if (preview.error)
+		return <div className='text-center font-semibold italic text-red-500'>{preview.error}</div>;
+	return <div className='text-center font-semibold italic text-green-700'>{preview.fileName}</div>;
+};
+
+const imgReader = (file: File): any =>
+	new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.addEventListener('load', () => {
+			resolve(reader.result);
+		});
+		reader.addEventListener('error', () => {
+			reject("couldn't load img");
+		});
+		reader.addEventListener('abort', () => {
+			reject('abort load img');
+		});
+		reader.readAsDataURL(file);
+	});
+
+export default UpdateProfilePic;
