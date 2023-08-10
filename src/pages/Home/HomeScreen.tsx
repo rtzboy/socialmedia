9555e1487a8fbdfa0ce8b1a45415950f50b7856e
Jhoseph Poma <japoma_200@hotmail.com>
2023-08-10 01:13:05 -0500

    ADD: FollowingList

--- src/pages/Home/HomeScreen.tsx
+++ src/pages/Home/HomeScreen.tsx
@@ -1,14 +1,16 @@
 import CreatePost from '../../components/homepage/CreatePost';
 import NewFeeds from '../../components/homepage/NewFeeds';
+import FriendList from './FriendList';
 
 const HomeScreen = () => {
 	return (
-		<>
-			<div className='mx-auto flex max-w-xl flex-col gap-4 p-4'>
+		<section className=''>
+			<div className='mx-auto flex w-full max-w-xl flex-col gap-4 p-4'>
 				<CreatePost makeFrom='Home' />
 				<NewFeeds />
 			</div>
-		</>
+			<FriendList />
+		</section>
 	);
 };
 
